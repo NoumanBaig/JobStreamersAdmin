005d3dc1e978252380777a9be95c9d94c84485c3
Nouman baig <nbaig3631@gmail.com>
2019-12-03 11:27:19 +0530

    Initial commit

---
+++ app/src/main/java/com/awt/jobstreamersadmin/rest/ApiInterface.java
@@ -0,0 +1,13 @@
+package com.awt.jobstreamersadmin.rest;
+
+
+public interface ApiInterface {
+//    @GET("movie/top_rated")
+//    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+//
+//    @GET("movie/{id}")
+//    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+
+//    @GET("/posts")
+//    Call<List<Post>> getPosts();
+}
---
+++ app/src/main/java/com/awt/jobstreamersadmin/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.awt.jobstreamersadmin.utils;
+
+public class Constants {
+    public static Boolean cons = false;
+    public static String string = "";
+}
